88ab83e74334f308fd7a5358ecaf9867cab93952
Fauzan Abdul Hakim <48903232+zhansky@users.noreply.github.com>
2020-01-05 13:34:21 +0700

    perkalian

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ for (var i = 1; i <= 5; i++) {
 	}
 	console.log(num);
 }
+
+//Perkalian
+var a = 1,
+    b = 2,
+    c = 3,
+    d = 4,
+    e = 5,
+    f = 6,
+    g = 7,
+    h = 8,
+    i = 9,
+    j = 10;
+
+    console.log("1x1 =",a*a),
+    console.log("1x2 =",a*b),
+    console.log("1x3 =",a*c),
+    console.log("1x4 =",a*d),
+    console.log("1x5 =",a*e),
+    console.log("1x6 =",a*f),
+    console.log("1x7 =",a*g),
+    console.log("1x8 =",a*h),
+    console.log("1x9 =",a*i),
+    console.log("1x10 =",a*j);
